c52aab596b331763c4aeedb951ac7c33e0d60aae
Leonardomottech <leonardomottech@gmail.com>
2026-06-18 16:36:24 -0300

    Versão final do banco de dados

--- campeonato-gui/executar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Compilando o projeto..."
-mkdir -p out
-javac -d out src/model/*.java src/service/*.java src/ui/Theme.java src/ui/LoginFrame.java src/ui/AdminFrame.java src/ui/ParticipantFrame.java src/Main.java
-echo "Iniciando..."
-java -cp out Main
